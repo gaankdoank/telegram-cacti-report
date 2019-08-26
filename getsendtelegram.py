bc61ac36f26eb64402a4250e3a3024b84b8edb65
gaankdoank <40822775+gaankdoank@users.noreply.github.com>
2019-08-26 10:46:51 +0700

    Update getsendtelegram.py

--- getsendtelegram.py
+++ getsendtelegram.py
@@ -5,13 +5,13 @@ import datetime
 from datetime import datetime
 from mysql.connector import Error
 
-bot = telebot.TeleBot("753045927:AAEOjo9tmoDBj0dUip9-qGg2jdGc2k5boWw")
+bot = telebot.TeleBot("YOUR-BOT-TOKEN")
 
 try:
  	connection = mysql.connector.connect(host='localhost',
                              database='cacti',
-                             user='root',
-                             password='ibs123')
+                             user='yourcactiuser',
+                             password='yourcactidb')
  	if connection.is_connected():
        		db_Info = connection.get_server_info()
        		print("Connected to MySQL database... MySQL Server version on ",db_Info)
@@ -29,7 +29,7 @@ try:
 	#		print(datetime)
 		print json.dumps(record, sort_keys=True, indent=4, separators=(',',':'))
 		#bot.send_message(287076189,"Automatic hosts Checked, the following offline devices : \n ============================= \n \n"+json.dumps(record,sort_keys=True, indent=4, separators=(',',':')))
-		bot.send_message(-367111902,"Automatic hosts Checked, the following offline devices : \n ============================= \n \n"+json.dumps(record,sort_keys=True, indent=4, separators=(',',':')))
+		bot.send_message(YOUR-CHAT-ID,"Automatic hosts Checked, the following offline devices : \n ============================= \n \n"+json.dumps(record,sort_keys=True, indent=4, separators=(',',':')))
 except Error as e :
     print ("Error while connecting to MySQL", e)
 finally:
